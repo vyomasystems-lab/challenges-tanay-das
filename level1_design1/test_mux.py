a9a677677514878d88825377581a920b685ea4de
Tanay Das <tanay2019das@gmail.com>
2022-07-20 16:12:50 +0000

    write testbench and update vyoma.sh

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,5 +6,11 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
+    sel1 = 5'b00000
+    sel2 = 5'b00010
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    dut.sel.value = sel1
+    cocotb.log.info(f'sel0={sel1} inp0={dut.inp0.value} model={dut.inp0.value} DUT={int(dut.out.value)}')
+
+    assert dut.out.value == dut.inp0.value, f"Adder result is incorrect: {dut.out.value} != {inp0}"
+
