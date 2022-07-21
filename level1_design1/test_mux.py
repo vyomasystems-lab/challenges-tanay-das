c06b374cb6b2f2cd483f50ff8852b226b56739c3
Tanay Das <tanay2019das@gmail.com>
2022-07-21 10:29:59 +0000

    working test

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,11 +6,12 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
-    sel1 = 5'b00000
-    sel2 = 5'b00010
-
-    dut.sel.value = sel1
-    cocotb.log.info(f'sel0={sel1} inp0={dut.inp0.value} model={dut.inp0.value} DUT={int(dut.out.value)}')
-
-    assert dut.out.value == dut.inp0.value, f"Adder result is incorrect: {dut.out.value} != {inp0}"
-
+    dut.inp0.value = 3
+    await Timer(2, units='ns')
+    dut.sel.value = 00000
+    
+    await Timer(5, units='ns')
+    a = dut.out.value
+    dut._log.info('hello this is the value')
+    dut._log.info(a)
+    dut._log.info(dut.inp0.value)
